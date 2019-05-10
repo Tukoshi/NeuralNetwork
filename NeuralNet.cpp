bf76ece355001b2de554a728543bcb08a9f89236
Tukoshi <33588001+Tukoshi@users.noreply.github.com>
2019-05-10 03:48:38 +0200

    Implementad backpropagation
    implementem backpropagation algorithm with no memory leaks,
    works fine but it needs to have deltaWeights scalled down by speed factor(argument of back propagation)

--- NeuralNet.cpp
+++ NeuralNet.cpp
@@ -44,6 +44,10 @@ NeuralNet::~NeuralNet()
 	{
 		delete this->weights.at(i);
 	}
+	for (int i = 0; i < this->topologySize - 1; i++)
+	{
+		delete this->GradientMatrices.at(i);
+	}
 }
 void NeuralNet::setCurrentInput(vector<double> input)
 {
@@ -93,6 +97,7 @@ void NeuralNet::printNet(int type = 0)
 	{
 		cout << "Layer #" << i + 1 << endl;
 		this->layers.at(i)->print(type);
+		cout<< endl;
 	}
 	cout << "-------------------\n";
 }
@@ -169,4 +174,167 @@ void NeuralNet::feedforward()
 		}
 		delete a, b, c;
 	}
+}
+
+void NeuralNet::setTarget(vector<double> target)
+{
+	this->target = target;
+}
+void NeuralNet::calculate_error()
+{
+	this->errors.clear();
+	this->error = 0.0;
+	int size = this->layers.at(this->topologySize - 1)->getSize();
+	Matrix * new_m = this->layers.at(this->topologySize - 1)->matrixifyActivated();
+
+	for (int i = 0; i < size; i++)
+	{
+		double tempErr = abs(new_m->get_value(0, i) - this->target.at(i));
+		this->errors.push_back(tempErr);
+		this->error += tempErr;
+	}
+}
+
+void NeuralNet::printError()
+{
+	cout << "Total Error: " << this->error << endl;
+	int size = this->errors.size();
+	for (int i = 0; i < size; i++)
+	{
+		cout << this->errors.at(i) << " ";
+	}
+	cout << endl;
+}
+void  NeuralNet::backpropagation(double speed = 0.00001)
+{
+	this->calculate_error();
+	vector <Matrix *> newWeights;
+	Matrix * gradients;
+
+	int OutLayerIndex = this->topologySize - 1;
+	Matrix * derivedvals = this->layers.at(OutLayerIndex)->matrixifyDerived();
+	Matrix * gradientYtoZ = new Matrix(1, this->layers.at(OutLayerIndex)->getSize(), false);
+	{
+		int size = this->errors.size();
+		for (int i = 0; i < size; i++)
+		{
+			gradientYtoZ->set_value(0, i, derivedvals->get_value(0, i) * errors.at(i));
+		}
+	}
+	int lastHiddenLayerIndex = OutLayerIndex - 1;
+	Layer * LastHiddenLayer = this->layers.at(lastHiddenLayerIndex);
+	Matrix * WeightsOutToHidden = this->weights.at(OutLayerIndex - 1);
+	gradientYtoZ->transpose();
+	Matrix * LastHiddenActivatedVals = LastHiddenLayer->matrixifyActivated();
+	Matrix * deltaOutToHidden = gradientYtoZ->multiplybymatrix(*LastHiddenActivatedVals);
+	gradientYtoZ->transpose();
+	Matrix * newWeightsOutToHiden = new Matrix(deltaOutToHidden->getNumRows(),
+		deltaOutToHidden->getNumCols(), 0.0);
+
+	for (int row = 0; row < deltaOutToHidden->getNumRows(); row++)
+	{
+		for (int col = 0; col < deltaOutToHidden->getNumCols(); col++)
+		{
+			double originalWeight = WeightsOutToHidden->get_value(row, col);
+			double deltaWeight = speed * deltaOutToHidden->get_value(row, col);
+			newWeightsOutToHiden->set_value(row, col, originalWeight - deltaWeight);
+		}
+	}
+
+	newWeights.push_back(newWeightsOutToHiden);
+
+	gradients = new Matrix(gradientYtoZ->getNumRows(), gradientYtoZ->getNumCols(), 0.0);
+
+	for (int row = 0; row < gradients->getNumRows(); row++)
+	{
+		for (int col = 0; col < gradients->getNumCols(); col++)
+		{
+			gradients->set_value(row, col, gradientYtoZ->get_value(row, col));
+		}
+	}
+	delete gradientYtoZ, newWeightsOutToHiden, deltaOutToHidden, LastHiddenActivatedVals;
+
+	for (int i = (OutLayerIndex - 1); i > 0; i--)
+	{
+		Layer *l = this->layers.at(i);
+		Matrix * derivedHidden = l->matrixifyDerived();
+		Matrix * ActivatedHidden = l->matrixifyActivated();
+		Matrix * derivedGradient = new Matrix(1, l->getSize(), 0.0);
+		Matrix * weightMatrix = this->weights.at(i);
+		Matrix * originalWeights = this->weights.at(i - 1);
+
+		{
+			int sizey = weightMatrix->getNumRows();
+			int sizex = weightMatrix->getNumCols();
+
+			for (int r = 0; r < sizey; r++)
+			{
+				double sum = 0.0;
+				for (int c = 0; c < sizex; c++)
+				{
+					sum += gradients->get_value(0, c) * weightMatrix->get_value(r, c);
+				}
+				double g = sum * ActivatedHidden->get_value(0, r);
+				derivedGradient->set_value(0, r, g);
+			}
+
+		}
+		Matrix * leftNeurons;
+		if ((i - 1) != 0)
+		{
+			leftNeurons = this->layers.at(i - 1)->matrixifyActivated();
+		}
+		else
+		{
+			leftNeurons = this->layers.at(i - 1)->matrixifyDelivered();
+		}
+
+		derivedGradient->transpose();
+		Matrix * deltaWeights = derivedGradient->multiplybymatrix(*leftNeurons);
+		Matrix * newWeightsHidden = new Matrix(deltaWeights->getNumRows(),
+			deltaWeights->getNumRows(),
+			0.0);
+		{
+			int sizey = deltaWeights->getNumRows();
+			int sizex = deltaWeights->getNumCols();
+			for (int r = 0; r < sizey; r++)
+			{
+				for (int c = 0; c < sizex; c++)
+				{
+					double w = originalWeights->get_value(r, c);
+					double d = speed * deltaWeights->get_value(r, c);
+					newWeightsHidden->set_value(r, c, w - d);
+				}
+			}
+		}
+		{
+			newWeights.push_back(newWeightsHidden);
+			delete gradients;
+			gradients = new Matrix(derivedGradient->getNumRows(),
+				derivedGradient->getNumCols(),
+				0.0);
+			int sizey = gradients->getNumRows();
+			int sizex = gradients->getNumCols();
+			for (int r = 0; r < sizey; r++)
+			{
+				for (int c = 0; c < sizex; c++)
+				{
+					gradients->set_value(r, c, derivedGradient->get_value(r, c));
+				}
+			}
+
+		}
+		delete derivedGradient, leftNeurons, deltaWeights, derivedHidden;
+	}
+	int size = this->weights.size();
+	for (int i = 0; i < size; i++)
+	{
+		delete this->weights.at(i);
+	}
+	this->weights.clear();
+	for (int i = newWeights.size() - 1; i >= 0; i--)
+	{
+		this->weights.push_back(newWeights.at(i));
+	}
+
 }
--- NeuralNet.h
+++ NeuralNet.h
@@ -11,12 +11,11 @@ public:
 	NeuralNet::NeuralNet(vector <int> topology, int function);
 	~NeuralNet();
 
-	void setCurrentInput(vector<double> input);
 	void printCurrentInput();
-
 	void printWeights(int index);
 	void printNet(int type);
 	void printLayer(int index, int type);
+	void printError();
 
 	Matrix * getNeuronMatrix(int index);
 	Matrix * getNeuronActMatrix(int index);
@@ -25,12 +24,25 @@ public:
 
 	void setNeuronValue(int layerIndex, int neuronIndex, double value, int function);
 	void setNeuronValue(int layerIndex, int neuronIndex, double value);
+	void setCurrentInput(vector<double> input);
+
 
 	void feedforward(int function_forward);
 	void feedforward();
 
+	void setTarget(vector<double> target);
+	void calculate_error();
+	
+	void backpropagation(double speed);
+	vector <Matrix *> GradientMatrices;
+	double error;
+
 private:
 	int chosen_act_func;
+	vector <double> errors;
+	vector <double> target;
+	vector <double> historicalError;
+
 	int topologySize;
 	vector <int>	  topology;
 	vector <Layer * > layers;
